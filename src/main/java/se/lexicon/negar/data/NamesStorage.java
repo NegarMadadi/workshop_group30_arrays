b1a1e982b1e57e8855eaadc90e5406ec079b17cd
NegarMadadi <madadi.negar@gmail.com>
2022-03-05 17:07:06 +0100

    Added add() in NamesStorage.

--- src/main/java/se/lexicon/negar/data/NamesStorage.java
+++ src/main/java/se/lexicon/negar/data/NamesStorage.java
@@ -1,10 +1,26 @@
 package se.lexicon.negar.data;
 
+import java.util.Arrays;
+
+/**
+ * @author Negar Madadi
+ */
 public class NamesStorage {
     private static String[] names = new String[0];
 
+    public static boolean add(final String fullName) {
+        if (fullName == null) {
+            return false;
+        }
+        if (nameExist(fullName)) {
+            return false;
+        }
+        names = Arrays.copyOf(names, names.length + 1);
+        names[names.length - 1] = fullName;
+        return true;
+    }
+
     /**
-     *
      * @param fullName String fullName
      * @return true if found.
      */
