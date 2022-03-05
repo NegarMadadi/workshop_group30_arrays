c6937b297846a5d4cc2661d682fe277ab9f1ae06
NegarMadadi <madadi.negar@gmail.com>
2022-03-05 16:50:23 +0100

    Added nameExists() in NamesStorage.

--- src/main/java/se/lexicon/negar/data/NamesStorage.java
+++ src/main/java/se/lexicon/negar/data/NamesStorage.java
@@ -3,6 +3,21 @@ package se.lexicon.negar.data;
 public class NamesStorage {
     private static String[] names = new String[0];
 
+    /**
+     *
+     * @param fullName String fullName
+     * @return true if found.
+     */
+    public static boolean nameExist(String fullName) {
+        boolean exists = false;
+        for (String name : names) {
+            if (name.equalsIgnoreCase(fullName)) {
+                exists = true;
+                break;
+            }
+        }
+        return exists;
+    }
 
     public static void clear() {
         names = new String[0];
